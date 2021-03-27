784e106aa109105cc54983f18a942065dfeb1eb1
Jeffery Bennett <meandmybadself@gmail.com>
2021-03-27 14:06:34 -0500

    Initial commit

---
+++ handler.js
@@ -0,0 +1,17 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Episodes.fm - root handler",
+  });
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+module.exports.handler = serverless(app);
