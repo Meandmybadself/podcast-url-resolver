05707e72de4bc95a7116949b389bc6fc6b60cb9b
Jeffery Bennett <meandmybadself@gmail.com>
2021-04-15 22:17:11 -0500

    debug: Adds prepush startup check

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -1,6 +1,6 @@
-/*eslint eslint-comments/no-unused-enable: error */
+/* eslint eslint-comments/no-unused-enable: error */
 
-import { Sequelize } from 'sequelize-typescript';
+import {Sequelize} from 'sequelize-typescript';
 import path from 'path';
 import Platform from './models/00-platform';
 import PlatformHost from './models/platform-host';
@@ -17,11 +17,11 @@ const sequelize = new Sequelize({
 
 const initializeBaseTables = async () => {
 	console.log('Initializing base tables.');
-	await Platform.create({ name: 'Overcast', platformId: 'overcast' });
-	await Platform.create({ name: 'Apple Podcasts', platformId: 'apple' });
+	await Platform.create({name: 'Overcast', platformId: 'overcast'});
+	await Platform.create({name: 'Apple Podcasts', platformId: 'apple'});
 
-	await PlatformHost.create({ hostname: 'overcast.fm', platformId: 1 });
-	await PlatformHost.create({ hostname: 'podcasts.apple.com', platformId: 2 });
+	await PlatformHost.create({hostname: 'overcast.fm', platformId: 1});
+	await PlatformHost.create({hostname: 'podcasts.apple.com', platformId: 2});
 	console.log('Done initializing base tables.');
 };
 
@@ -31,18 +31,18 @@ const initializeBaseTables = async () => {
 	console.info('Connected to database.');
 
 	if (process.env.DB_INIT_BASE_TABLES === '1') {
-		await sequelize.sync({ force: true });
+		await sequelize.sync({force: true});
 		await initializeBaseTables();
 	} else {
 		console.log('Altering tables.');
-		await sequelize.sync({ alter: true });
+		await sequelize.sync({alter: true});
 	}
 
 	if (process.env.PREPUSH_CHECK) {
 		console.log('In PREPUSH check. Successfully started. Exiting.');
-		/*eslint-disable unicorn/no-process-exit */
+		/* eslint-disable unicorn/no-process-exit */
 		process.exit();
-		/*eslint-enable unicorn/no-process-exit */
+		/* eslint-enable unicorn/no-process-exit */
 	}
 })();
 
