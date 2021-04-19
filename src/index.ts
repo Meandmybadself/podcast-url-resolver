438f8828ffc5dc6967ad1c6fc03130b0f3db94f7
Jeffery Bennett <meandmybadself@gmail.com>
2021-04-18 22:00:04 -0500

    Implements raw HTTP request for Overcast auth cookie

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-if (process.env.DB_HOST) {
-	console.info('Initializing with supplied environment variables.');
-} else {
-	console.info('Initialzing with local environment variables.');
-	require('dotenv').config();
-}
-
 console.log(`🎧 Episodes.fm - ${process.env.ENV}`);
 
 import sequelize from './sequelize';
