1f0cab828c6d37da3fa15956759d15885465d273
Jeffery Bennett <meandmybadself@gmail.com>
2021-04-19 10:42:11 -0500

    Adds Google Podcasts to platforms list

--- src/routes/platform.ts
+++ src/routes/platform.ts
@@ -4,6 +4,12 @@ import sequelize from '../sequelize';
 const routes = (router: Router) => {
 	router.get('/platforms', async (_request: Request, response: Response) => {
 		const result: any = await sequelize.model('Platform').findAll({attributes: ['name', 'platformId']});
+
+		result.push({
+			name: 'Google Podcasts',
+			id: 'google'
+		});
+
 		return response.status(200).json({
 			message: 'ok',
 			result
@@ -12,6 +18,7 @@ const routes = (router: Router) => {
 	router.post('/platform', async (request: Request, response: Response) => {
 		const {name, platformId} = request.body;
 		const result = await sequelize.model('Platform').create({name, platformId});
+
 		return response.status(200).json({
 			message: 'ok',
 			result
