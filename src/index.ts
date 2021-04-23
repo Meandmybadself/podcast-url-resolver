678385d39192a079fc79afbc02caa40330f37115
Jeffery Bennett <meandmybadself@gmail.com>
2021-04-23 12:09:42 -0500

    Adds initial user logging

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,27 @@ console.log(`🎧 Episodes.fm - ${process.env.ENV}`);
 import sequelize from './sequelize';
 import {initExpress} from './express';
 
+import jwt from 'jsonwebtoken';
+import User from './models/user';
+
 (async () => {
 	try {
 		initExpress();
+
+		// Log user JWTs
+		(await User.findAll())
+			.map(user => user.get({plain: true}))
+			.forEach(user => {
+				const payload = {
+					userId: user.id,
+					role: user.role
+				};
+				const secret = jwt.sign(payload, process.env.JWT_SECRET, {
+					algorithm: 'HS256'
+				});
+
+				console.log(user.email, secret);
+			});
 	} catch (error: unknown) {
 		console.error(error);
 	}
