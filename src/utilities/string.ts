99327a56ac0065613196a0cbea5e1d796a89ecb0
Jeffery Bennett <meandmybadself@gmail.com>
2021-04-29 14:48:14 -0500

    Reduces search string length to 30 (from no limit)

--- src/utilities/string.ts
+++ src/utilities/string.ts
@@ -1,8 +1,12 @@
-export const makeSearchSafeString = (string: string): string => string.toLowerCase().replace(/[\W|aeiou]+/g, '');
+export const makeSearchSafeString = (string: string): string =>
+  string.toLowerCase().replace(/[\W|aeiou]+/g, "");
 
-export const normalizeText = (string: string): string => string.replace('–', '-') // Em dash
-	.replace('“', '"') // Smart quotes
-	.replace('‘', '\'')
-	.replace('”', '"')
-	.replace('’', '\'')
-	.trim();
+export const normalizeText = (string: string): string =>
+  string
+    .replace("–", "-") // Em dash
+    .replace("“", '"') // Smart quotes
+    .replace("‘", "'")
+    .replace("”", '"')
+    .replace("’", "'")
+    .trim()
+    .substr(0, 30);
