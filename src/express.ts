12fd9842fbedc48027829992eb7b19c6f3bf62b3
Jeffery Bennett <meandmybadself@gmail.com>
2021-04-29 21:38:04 -0500

    Adds helmet

--- src/express.ts
+++ src/express.ts
@@ -1,57 +1,60 @@
-import bodyParser from 'body-parser';
-import express, {NextFunction, Request, Response} from 'express';
-import errorhandler from 'strong-error-handler';
-import path from 'path';
-import cors from 'cors';
+import bodyParser from "body-parser";
+import express, { Request, Response } from "express";
+import errorhandler from "strong-error-handler";
+import path from "path";
+import helmet from "helmet";
 
-import episodeRoutes from './routes/episode';
-import platformRoutes from './routes/platform';
-import podcastRoutes from './routes/podcast';
-import userRoutes from './routes/user';
+import episodeRoutes from "./routes/episode";
+import platformRoutes from "./routes/platform";
+import podcastRoutes from "./routes/podcast";
+import userRoutes from "./routes/user";
 
-import * as requestResponseMiddleware from './utilities/request-response';
+import * as requestResponseMiddleware from "./utilities/request-response";
 
-export const initExpress = () => {
-	const app = express();
+export const initExpress = (): void => {
+  const app = express();
 
-	app.use(cors());
-	app.options('*', cors());
+  const router = express.Router();
 
-	const router = express.Router();
+  app.use(
+    helmet({
+      contentSecurityPolicy: false,
+    })
+  );
+  app.use(requestResponseMiddleware.attachHelpers);
 
-	app.use(requestResponseMiddleware.attachHelpers);
+  app.get("/", (_request: Request, response: Response) => {
+    response.redirect("/v1/");
+  });
 
-	app.get('/', (_request: Request, response: Response) => {
-		response.redirect('/v1/');
-	});
+  router.use(express.static(path.join(__dirname, "/public")));
+  router.use(bodyParser.json());
 
-	router.use(express.static(path.join(__dirname, '/public')));
-	router.use(bodyParser.json());
+  router.get("/", (_request: Request, response: Response) => {
+    response.status(200).sendFile(path.join(__dirname, "public/index.html"));
+  });
 
-	router.get('/', (_request: Request, response: Response) => {
-		response.status(200).sendFile(path.join(__dirname, 'public/index.html'));
-	});
+  episodeRoutes(router);
+  platformRoutes(router);
+  podcastRoutes(router);
+  userRoutes(router);
 
-	episodeRoutes(router);
-	platformRoutes(router);
-	podcastRoutes(router);
-	userRoutes(router);
+  router.use((_request: Request, response: Response) => {
+    return response.status(404).json({
+      error: "Not Found",
+    });
+  });
 
-	router.use((_request: Request, response: Response) => {
-		return response.status(404).json({
-			error: 'Not Found'
-		});
-	});
+  router.use(
+    errorhandler({
+      debug: process.env.ENV !== "prod",
+      log: true,
+    })
+  );
 
-	router.use(errorhandler({
-		debug: process.env.ENV !== 'prod',
-		log: true
-	}));
+  app.use("/v1", router);
 
-	app.use('/v1', router);
-
-	const port: number = Number.parseInt(process.env.PORT, 10) || 8000;
-	app.listen(port);
-	console.info(`⚡ Server started: http://127.0.0.1:${port}`);
+  const port: number = Number.parseInt(process.env.PORT, 10) || 8000;
+  app.listen(port);
+  console.info(`⚡ Server started: http://127.0.0.1:${port}`);
 };
-
